0162055697a3a1cd4c7df916ef5966a15b4c4dcc
mylibh <gav503244@gmail.com>
2020-09-08 16:27:57 +0300

    [3sem][1lab] fixed Utility filename

--- 3 sem/1/src/main.cpp
+++ 3 sem/1/src/main.cpp
@@ -1,5 +1,7 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com#include "Utiliry.hpp"
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#include "Utility.hpp"
 
 #include <memory>
 #include <map>
