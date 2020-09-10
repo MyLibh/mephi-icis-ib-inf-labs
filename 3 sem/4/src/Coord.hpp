76aaebb670c6bae176a752d7292d40561160b42e
mylibh <gav503244@gmail.com>
2020-09-10 18:11:16 +0300

    [3sem][4lab] docs
    + Coord

--- 3 sem/4/src/Coord.hpp
+++ 3 sem/4/src/Coord.hpp
@@ -1,3 +1,15 @@
+/**
+ * @file Coord.hpp
+ *
+ * @ingroup Utility
+ *
+ * @brief Provides an implementation of point
+ *
+ * @note Assumes that each coordinate is in range [0; 2^32)
+ *
+ * @author mylibh
+*/
+
 #ifndef __COORD_HPP_INCLUDED__
 #define __COORD_HPP_INCLUDED__
 
@@ -11,16 +23,46 @@ namespace MobileRobots
 		uint32_t y;
 	};
 
+	/**
+	 * @brief Compares tow points
+	 * 
+	 * @param lhs First instance
+	 * @param rhs Second instance
+	 * 
+	 * @return True if 'lhs' is to the left or to the bottom, False overwise
+	 * 
+	 * @exceptsafe
+	*/
 	inline constexpr bool operator<(const Coord& lhs, const Coord& rhs) noexcept
 	{
 		return (lhs.x < rhs.x || (lhs.x == rhs.x && lhs.y < rhs.y)) ? true : false;
 	}
 
+	/**
+	 * @brief Compares two points
+	 * 
+	 * @param lhs First instance
+	 * @param rhs Second instance
+	 * 
+	 * @return True if they are the same, False overwise
+	 * 
+	 * @exceptsafe
+	*/
 	inline constexpr bool operator==(const Coord& lhs, const Coord& rhs) noexcept
 	{
 		return lhs.x == rhs.x && lhs.y == rhs.y;
 	}
 
+	/**
+	 * @brief Compares two points
+	 * 
+	 * @param lhs First instance
+	 * @param rhs Second instance
+	 * 
+	 * @return True if they are not the same, False overwise
+	 * 
+	 * @exceptsafe
+	*/
 	inline constexpr bool operator!=(const Coord& lhs, const Coord& rhs) noexcept
 	{
 		return !(lhs == rhs);
@@ -29,14 +71,19 @@ namespace MobileRobots
 
 namespace std
 {
+	/**
+	 * @brief Specialization of std::hash for MobileRobots::Coord
+	 * 
+	 * @exceptsafe
+	*/
 	template<>
 	struct hash<MobileRobots::Coord>
 	{
-		inline constexpr size_t operator()(const MobileRobots::Coord& coord) const
+		inline constexpr size_t operator()(const MobileRobots::Coord& coord) const noexcept
 		{
 			return (static_cast<size_t>(coord.x) << 32) | static_cast<size_t>(coord.y); //-V112
 		}
 	};
-}
+} // namespace std
 
 #endif /* !__COORD_HPP_INCLUDED__ */
