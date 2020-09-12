3d24836a1603c0b95bbfaa946bb3acff59260d44
mylibh <gav503244@gmail.com>
2020-09-12 05:02:45 +0300

    [3sem][4lab] reverse range-based
    + config update

--- 3 sem/4/src/Utility.hpp
+++ 3 sem/4/src/Utility.hpp
@@ -5,6 +5,30 @@
 
 namespace MobileRobots::Utility
 {
+	template <typename _Ty>
+	struct ReverseWrapper 
+	{ 
+		_Ty& iterable;
+	};
+
+	template <typename _Ty>
+	inline auto begin(ReverseWrapper<_Ty> rev) 
+	{ 
+		return std::rbegin(rev.iterable); 
+	}
+
+	template <typename _Ty>
+	auto end(ReverseWrapper<_Ty> rev)
+	{ 
+		return std::rend(rev.iterable); 
+	}
+
+	template <typename _Ty>
+	inline ReverseWrapper<_Ty> reverse(_Ty&& iterable) 
+	{ 
+		return { iterable }; 
+	}
+
 	void showError(const std::string_view msg);
 } // namespace MobileRobots::Utility
 
